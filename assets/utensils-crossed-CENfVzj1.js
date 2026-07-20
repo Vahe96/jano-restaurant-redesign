aa4e7d700877639d549ea9375d8a3a8863ac74fc
vahagn <vahagn258@gmail.com>
2026-07-20 23:12:53 +0400

    Deploy box sliders

--- assets/utensils-crossed-CENfVzj1.js
+++ assets/utensils-crossed-CENfVzj1.js
@@ -1,4 +1,4 @@
-import{c as e}from"./SiteChrome-QOt3jhRd.js";/**
+import{c as e}from"./SiteChrome-CLCp5Fil.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
