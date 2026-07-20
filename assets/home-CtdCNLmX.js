42791969b145468d77ea082c881304576a30a09d
vahagn <vahagn258@gmail.com>
2026-07-20 17:55:38 +0400

    Deploy category and box experience

---
+++ assets/home-CtdCNLmX.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/main-DqaEeAXD.js","assets/SiteChrome-jqc8myNm.js","assets/SiteChrome-BETSVnnX.css","assets/package-open-jxDOxye1.js"])))=>i.map(i=>d[i]);
+import"./modulepreload-polyfill-B5Qt9EMX.js";import{_}from"./preload-helper-CDDSECo5.js";_(()=>import("./main-DqaEeAXD.js"),__vite__mapDeps([0,1,2,3]));
