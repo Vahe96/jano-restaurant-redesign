08f362fbe336b3e04471c636d17d4e52b9ed6fb8
vahagn <vahagn258@gmail.com>
2026-07-20 17:13:55 +0400

    Deploy Jano redesign

---
+++ assets/home-XmBdsbuj.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/main-C8cq_PCO.js","assets/SiteChrome-DFPqot6-.js","assets/SiteChrome-B7dSWVeH.css"])))=>i.map(i=>d[i]);
+import{_}from"./preload-helper-BU6YqacT.js";_(()=>import("./main-C8cq_PCO.js"),__vite__mapDeps([0,1,2]));
---
+++ assets/menu-DhMkJqmj.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/menuEntry-BTIV3PL5.js","assets/SiteChrome-DFPqot6-.js","assets/SiteChrome-B7dSWVeH.css"])))=>i.map(i=>d[i]);
+import{_}from"./preload-helper-BU6YqacT.js";_(()=>import("./menuEntry-BTIV3PL5.js"),__vite__mapDeps([0,1,2]));
